5b5ed84250611848d97bf26023c6cb8471a0ed45
jorgeacosta02 <jorgeacostadeleon11@yahoo.com>
2023-12-12 16:35:14 -0300

    Functions

--- src/Content.js
+++ src/Content.js
@@ -4,13 +4,29 @@ const Content = () => {
         const names = ['Jorge','Juan','Marti']
         const int = Math.floor(Math.random() * 3)
         return names[int];
-      }
+    }
+
+    const handleClick = () => {
+        console.log('You clicked it')
+    }
+
+    const handleClick2 = (name) => {
+        console.log(`${name} was clicked`)
+    }
+
+    const handleClick3 = (e) => {
+        console.log(e.target.innerText)
+    }
 
     return(
         <main>
-            <p>
+            <p onDoubleClick={handleClick}>
                 Hello {handleNameChange()}
             </p>
+            <button onClick={handleClick}>Click it</button>
+            <button onClick={() => handleClick2('Juan')}>Click it 2</button>
+            <button onClick={(e) => handleClick3(e)}>Click it 
+            3</button>
         </main>
     )
 }
