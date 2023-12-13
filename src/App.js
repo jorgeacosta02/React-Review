f93da592db1cd8a95f09620dbe848b9f3a1ebd59
jorgeacosta02 <jorgeacostadeleon11@yahoo.com>
2023-12-13 17:43:30 -0300

    Search Items

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Header from './Header';
+import SearchItem from './SearchItem';
 import AddItem from './AddItem';
 import Content from './Content';
 import Footer from './Footer';
@@ -9,6 +10,7 @@ function App() {
   const [items, setItems] = useState(JSON.parse(localStorage.getItem('shoppinglist')));
 
   const [newItem, setNewItem] = useState('');
+  const [search, setSearch] = useState('');
 
   const setAndSaveItems = (newItems) => {
     setItems(newItems);
@@ -47,7 +49,11 @@ function App() {
         setNewItem={setNewItem}
         handleSubmit={handleSubmit}  
       />
-      <Content items={items}
+      <SearchItem
+        search={search}
+        setSearch={setSearch}
+      />
+      <Content items={items.filter(item => ((item.item).toLowerCase()).includes(search.toLowerCase()))}
         handleCheck={handleCheck}
         handleDelete={handleDelete}
       />
